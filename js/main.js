3ff9d9bce196a7e8aa7dce72f04d1ba257debed5
Daniil <daniil.shishaev@gmail.com>
2025-01-17 01:21:58 +0300

    vertical line + some icons

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('.burger');
-    const sidebar = document.querySelector('.sidebar');
     const closeBtn = document.querySelector('.sidebar__close');
-    const header = document.querySelector('.header');
-    const mainContent = document.querySelector('.main-content');
+    const sidebar = document.querySelector('.sidebar');
+    const contentWrapper=document.querySelector('.content-wrapper');
 
     burger.addEventListener('click', function() {
         sidebar.classList.add('sidebar--active');
-        header.classList.add('blur');
-        mainContent.classList.add('blur');
-    });
+        contentWrapper.classList.add('blur');
+     });
 
     closeBtn.addEventListener('click', function() {
         sidebar.classList.remove('sidebar--active');
-        header.classList.remove('blur');
-        mainContent.classList.remove('blur');
-    });
+        contentWrapper.classList.remove('blur');
+     });
 });
